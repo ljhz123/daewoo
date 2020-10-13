008612748be215067667c9d0872654047d591683
lepoeme20 <lepoeme20@gmail.com>
2020-10-13 18:49:37 +0900

    multiple parmas in p.map

--- preprocessing/save_data.py
+++ preprocessing/save_data.py
@@ -8,8 +8,9 @@ from multiprocessing import Pool
 from collections import defaultdict
 from functools import partial
 
-def preprocessing(img_name, data_path, folder):
+def preprocessing(img_name, data_path, folder, save_path):
     try:
+        print(img_name, data_path, folder, save_path)
         img = cv2.imread(os.path.join(data_path, folder, img_name), cv2.IMREAD_GRAYSCALE)
         img_gray = img.astype(np.float32)
 
@@ -32,16 +33,16 @@ if __name__=='__main__':
         remove_img_dict[k].append(tmp)
 
     # set data_path
-    data_path = '/media/lepoeme20/Data/projects/daewoo/brave/data'
-    save_path = '/media/lepoeme20/Data/projects/daewoo/brave/crop'
-    folders = sorted(os.listdir(data_path))
+    d_path = '/media/lepoeme20/Data/projects/daewoo/brave/data'
+    s_path = '/media/lepoeme20/Data/projects/daewoo/brave/crop'
+    folders = sorted(os.listdir(d_path))
 
     for i, folder in enumerate(folders):
         # create save path
-        os.makedirs(os.path.join(save_path, folder), exist_ok=True)
+        os.makedirs(os.path.join(s_path, folder), exist_ok=True)
 
         # get images
-        all_imgs = sorted(os.listdir(os.path.join(data_path, folder)))
+        all_imgs = sorted(os.listdir(os.path.join(d_path, folder)))
         imgs = list(filter(lambda x: 7 <= int(x[8:10]) < 17, all_imgs))
 
         rm_imgs = []
@@ -52,9 +53,9 @@ if __name__=='__main__':
                 rm_imgs.extend(list(filter(lambda x: start <= x[:-4] <= end, imgs)))
         imgs = list(filter(lambda x: x not in rm_imgs, imgs))
 
-        save_img=partial(preprocessing, data_path=data_path, folder=folder)
+        save_img=partial(preprocessing, data_path=d_path, folder=folder, save_path=s_path)
 
-        with Pool(32) as p:
+        with Pool(16) as p:
             p.map(save_img, imgs)
 
         print("{}/{} \t Folder: {} | Total Image: {:,} | RM Image: {:,} | Usable Image: {:,} | time: {:.3f}s".format(
